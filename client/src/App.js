cb52a471b5b601e81a6814dc56dd0e57e518d591
devartist <kevinwasonga415@outlook.com>
2022-10-02 09:32:17 +0300

    delopy

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
+import {useState, useEffect} from "react"
+import axios from "axios"
+
 function App() {
+ const [data, setData] = useState(null)
+
+ async function fetchData() {
+  try {
+   const res = await axios.get("/api")
+
+   setData(res.data.message)
+  } catch (error) {
+   console.log(error)
+  }
+ }
+
+ useEffect(() => {
+  fetchData()
+ }, [])
+
  return (
   <div className="App">
-   <h1>Hello world</h1>
+   <h1>{data}</h1>
   </div>
  )
 }
